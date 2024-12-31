94f04d9a34396541e1050f68327edef8c776828b
T Shrinivasan <tshrinivasan@gmail.com>
2024-12-31 18:07:30 -0500

    added 12-list.py

---
+++ 12-list.py
@@ -0,0 +1,31 @@
+# list
+# in this code, we explore the list
+
+# this is a variable
+# variable has only only value
+name = "viyan"
+age = 10
+
+
+# this is a list
+# a list can have multiple values
+friends = ["nithya","bala","david","ravi","anwar"]
+
+print(friends)
+print(friends[0])
+print(friends[3])
+print(friends[-1])
+print(friends[-2])
+
+print(len(friends))
+
+friends.sort()
+print(friends)
+
+
+friends.reverse()
+print(friends)
+
+for item in friends:
+    print("my friend name is ", item)
+
